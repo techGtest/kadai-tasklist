25a434e0800a8fffe8e12aa486b20fe84bc411cb
tech.G.test <tech.G.test@gmail.com>
2021-04-29 17:30:55 +0000

    tasklist before submit-1

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task; 
+
+//Model名：Task(Modelのclass名も同)
+//$URI：/tasks(ルーティング設定済)
+
+class TasksController extends Controller
+{
+    public function index() //一覧を表示
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index', ['tasks'=>$tasks,]);
+    }
+
+    public function create() //入力フォームを表示
+    {
+        $task = new Task;
+        
+        return view('tasks.create', ['task'=>$task,]);
+    }
+
+    public function store(Request $request) //投稿機能
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    
+    public function show($id) //詳細ページ表示
+    {
+        $task = Task::findOrFail($id);
+        
+        return view('tasks.show', ['task'=>$task,]);
+    }
+
+    
+    public function edit($id) //編集ページ表示
+    {
+        $task = Task::findOrFail($id);
+        
+        return view('tasks.edit', ['task'=>$task,]);
+        
+    }
+
+   
+    public function update(Request $request, $id) //編集機能
+    {
+        $task = Task::findOrFail($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    
+    public function destroy($id) //削除機能
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        
+         return redirect('/');
+    }
+}
